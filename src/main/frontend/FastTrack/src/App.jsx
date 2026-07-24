eba4ba8b82efc73d866d7a691d0c827513ff916c
Lawrence <lawrencesanzogni@gmail.com>
2026-07-24 11:08:39 -0500

    Sprint_1(frontend): added context state for user data

--- src/main/frontend/FastTrack/src/App.jsx
+++ src/main/frontend/FastTrack/src/App.jsx
@@ -1,12 +1,15 @@
-import {useEffect, useState} from "react";
+import {useEffect, useState, useContext, createContext} from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from './Components/Home.jsx'
 import Checkout from './Checkout.jsx'
 import Login from "./Components/Login.jsx";
 
+export const UserContext = createContext();
+
 function App() {
     const [productList, setProductList] = useState([]);
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [user, setUser] = useState({});
 
     useEffect(() => {
 
@@ -15,11 +18,13 @@ function App() {
     return (
         <div className="App">
             <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Home/>} />
-                    <Route path="/checkout" element={<Checkout/>} />
-                    <Route path="/login" element={<Login/>} />
-                </Routes>
+                <UserContext.Provider value={{user, setUser}}>
+                    <Routes>
+                        <Route path="/" element={<Home/>} />
+                        <Route path="/checkout" element={<Checkout/>} />
+                        <Route path="/login" element={<Login />} />
+                    </Routes>
+                </UserContext.Provider>
             </BrowserRouter>
         </div>
     );
--- src/main/frontend/FastTrack/src/Components/Login.jsx
+++ src/main/frontend/FastTrack/src/Components/Login.jsx
@@ -1,17 +1,28 @@
 import './Navbar.jsx'
 import Navbar from "./Navbar.jsx";
+import {useContext, useState} from "react";
+import {UserContext} from "../App.jsx"
 
 function Login() {
+    const user = useContext(UserContext);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleUser = (e) => {
+        e.preventDefault();
+        setUser({...user, [email]: e.target.value, password: password});
+    };
+
     return (
         <div className="login">
             <Navbar/>
             <h1>Login page</h1>
-            <form className="login-form">
+            <form className="login-form" onSubmit={handleUser}>
                 <div className="form-group">
-                    <input type="email" placeholder="Email" />
+                    <input type="email" placeholder="Email" onChange={e => setEmail(e.target.value)} />
                 </div>
                 <div className="form-group">
-                    <input type="password" placeholder="Password" />
+                    <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
                 </div>
                 <button type="submit" className="btn btn-primary">Login</button>
             </form>
